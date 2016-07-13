4a1bdaff1f196a08236412756765bf015af65757
Archana Dharmavaram <archie146@gmail.com>
2016-07-13 17:54:30 -0400

    Sort Functionality

--- js/app.js
+++ js/app.js
@@ -16,7 +16,57 @@ app.controller('mainCtrl', function($scope){
 			artist: "tyagaraja",
 			album: "xyz",
 			rating: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "kannada",
+			artist: "tyagaraja",
+			album: "xyz",
+			rating: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "Hindi",
+			artist: "S.D Burman",
+			album: "Guide",
+			rating: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "telugu",
+			artist: "tyagaraja",
+			album: "shenai",
+			rating: 10
+		},
+		{
+			name: "Kambhoji",
+			lang: "telugu",
+			artist: "Ravi Shankar",
+			album: "Sitar",
+			rating: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "telugu",
+			artist: "A R Rahman",
+			album: "xyz",
+			rating: 10
+		},
+		{
+			name: "Kalyani",
+			lang: "telugu",
+			artist: "Annamayya",
+			album: "xyz",
+			rating: 10
 		}
 	];
+
+	$scope.propertyName = 'name';
+	$scope.reverse = false;
+
+	$scope.sortBy = function(propertyName){
+		$scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+		$scope.propertyName = propertyName;
+	};
 
 });
