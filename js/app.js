04e0ec6412d9402a2b4fed64da2e8fc75694b486
Archana Dharmavaram <archie146@gmail.com>
2016-07-14 14:56:56 -0400

    Dynamic ng-model creation, removed hardcoded header sort and search

--- js/app.js
+++ js/app.js
@@ -2,62 +2,186 @@ var app = angular.module('raagApp', []);
 
 app.controller('mainCtrl', function($scope){
 
+	$scope.query = {};
+	// console.log($scope.query);
+	$scope.header = [
+		{
+			displayName: "Raaga",
+		  colName: "name"
+		},
+		{
+		  displayName: "Language",
+		  colName: "lang"
+		},
+		{ displayName: "Artist",
+			colName: "artist"
+		},
+		{ displayName: "Album",
+			colName: "album"
+		},
+		{
+			displayName: "No. of Hits",
+			colName: "hits"
+		},
+		{ displayName: "Listen",
+			colName: "link"
+		}
+	];
+		
 	$scope.raagas = [ 
 		{
 			name: "hamsadhwani",
 			lang: "hindi",
 			artist: "abc",
 			album: "xyz",
-			rating: 10
+			hits: 10
 		},
 		{
 			name: "bhairavi",
 			lang: "telugu",
 			artist: "tyagaraja",
 			album: "xyz",
-			rating: 10
+			hits: 10
 		},
 		{
 			name: "bhairavi",
 			lang: "kannada",
 			artist: "tyagaraja",
 			album: "xyz",
-			rating: 10
+			hits: 10
 		},
 		{
 			name: "bhairavi",
 			lang: "Hindi",
 			artist: "S.D Burman",
 			album: "Guide",
-			rating: 10
+			hits: 10
 		},
 		{
 			name: "bhairavi",
 			lang: "telugu",
 			artist: "tyagaraja",
 			album: "shenai",
-			rating: 10
+			hits: 10
 		},
 		{
 			name: "Kambhoji",
 			lang: "telugu",
 			artist: "Ravi Shankar",
 			album: "Sitar",
-			rating: 10
+			hits: 10
 		},
 		{
 			name: "bhairavi",
 			lang: "telugu",
 			artist: "A R Rahman",
 			album: "xyz",
-			rating: 10
+			hits: 10
 		},
 		{
 			name: "Kalyani",
 			lang: "telugu",
 			artist: "Annamayya",
 			album: "xyz",
-			rating: 10
+			hits: 10
+		},
+				{
+			name: "bhairavi",
+			lang: "telugu",
+			artist: "tyagaraja",
+			album: "xyz",
+			hits: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "kannada",
+			artist: "tyagaraja",
+			album: "xyz",
+			hits: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "Hindi",
+			artist: "S.D Burman",
+			album: "Guide",
+			hits: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "telugu",
+			artist: "tyagaraja",
+			album: "shenai",
+			hits: 10
+		},
+		{
+			name: "Kambhoji",
+			lang: "telugu",
+			artist: "Ravi Shankar",
+			album: "Sitar",
+			hits: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "telugu",
+			artist: "A R Rahman",
+			album: "xyz",
+			hits: 10
+		},
+		{
+			name: "Kalyani",
+			lang: "telugu",
+			artist: "Annamayya",
+			album: "xyz",
+			hits: 10
+		},
+				{
+			name: "bhairavi",
+			lang: "telugu",
+			artist: "tyagaraja",
+			album: "xyz",
+			hits: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "kannada",
+			artist: "tyagaraja",
+			album: "xyz",
+			hits: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "Hindi",
+			artist: "S.D Burman",
+			album: "Guide",
+			hits: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "telugu",
+			artist: "tyagaraja",
+			album: "shenai",
+			hits: 10
+		},
+		{
+			name: "Kambhoji",
+			lang: "telugu",
+			artist: "Ravi Shankar",
+			album: "Sitar",
+			hits: 10
+		},
+		{
+			name: "bhairavi",
+			lang: "telugu",
+			artist: "A R Rahman",
+			album: "xyz",
+			hits: 10
+		},
+		{
+			name: "Kalyani",
+			lang: "telugu",
+			artist: "Annamayya",
+			album: "xyz",
+			hits: 10
 		}
 	];
 
@@ -70,3 +194,17 @@ app.controller('mainCtrl', function($scope){
 	};
 
 });
+
+app.directive('dynamicModel', ['$compile', '$parse', function ($compile, $parse) {
+    return {
+        restrict: 'A',
+        terminal: true,
+        priority: 100000,
+        link: function (scope, elem) {
+            var name = $parse(elem.attr('dynamic-model'))(scope);
+            elem.removeAttr('dynamic-model');
+            elem.attr('ng-model', name);
+            $compile(elem)(scope);
+        }
+    };
+}]);
